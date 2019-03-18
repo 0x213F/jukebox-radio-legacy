a0c1c55d62fcd6ec19e66a0803ccb5a9a520ba38
Josh Schultheiss <joshschultheiss@Joshs-iMac.local>
2019-03-17 23:31:32 -0700

    changed public dir location

--- proj/settings.py
+++ proj/settings.py
@@ -136,4 +136,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = 'public/'
-STATIC_ROOT = f'{BASE_DIR}/proj/site/static/public'
+STATIC_ROOT = f'{BASE_DIR}/proj/site/public/'
