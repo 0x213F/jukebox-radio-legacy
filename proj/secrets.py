f4c2ab1b21b4c4c6c15c80b51c992fd70158e36b
Josh <josh@schultheiss.io>
2019-09-09 06:08:26 +0000

    more changes

--- proj/secrets.py
+++ proj/secrets.py
@@ -1,6 +1,7 @@
 SERVER_IP = '157.245.173.147'
+SERVER_URL = 'jukeboxrad.io'
 
-DB_HOST_URL = 'private-db-postgresql-sfo2-61001-do-user-6189280-0.db.ondigitalocean.com'
+DB_HOST_URL = 'db-postgresql-sfo2-61001-do-user-6189280-0.db.ondigitalocean.com'
 DB_HOST_PORT = 25060
 DB_NAME = 'musicappdb'
 DB_USER = 'musicappadmin'
@@ -13,3 +14,4 @@ SPOTIFY_CLIENT_ID = ''
 SPOITFY_CLIENT_SECRET = ''
 SPOTIFY_REDIRECT_URI = ''
 SPOTIFY_USER_DATA_SCOPES = 'streaming app-remote-control user-modify-playback-state user-read-currently-playing user-read-playback-state'
+
