a1184d8a161faed214e70dbae189c0db35b3baed
Josh Schultheiss <joshschultheiss@joshs-imac.lan>
2019-12-08 03:01:53 -0800

    v commit

---
+++ proj/apps/users/management/commands/idle_showings.py
@@ -0,0 +1,23 @@
+
+from datetime import datetime
+from datetime import timedelta
+
+from django.apps import apps
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    help = 'Idle showings'
+
+    def handle(self, *args, **options):
+        Showing = apps.get_model('music.Showing')
+
+        showings_to_idle = (
+            Showing
+            .objects
+            .filter(
+                status=Showing.STATUS_ACTIVATED,
+                record_terminates_at__lt=now - timedelta(minutes=5),
+            )
+        )
+        showings_to_idle.update(status=Showing.STATUS_IDLE)
