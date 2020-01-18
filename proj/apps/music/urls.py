fad076fa9df1237bde47228f1b8dad204bd46d9c
Josh Schultheiss <joshschultheiss@joshs-imac.lan>
2020-01-18 13:55:18 -0800

    35: GET music/api/list_broadcasting_streams

--- proj/apps/music/urls.py
+++ proj/apps/music/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
 from .views import CreateStreamView
+from .views import ListBroadcastingStreamsView
 from .views import ListStreamsView
 from .views import SpinRecordView
 from .views import StreamSubscriptionView
@@ -8,6 +9,7 @@ from .views import StreamSubscriptionView
 
 urlpatterns = [
     path("create_stream/", CreateStreamView.as_view()),
+    path("list_broadcasting_streams/", ListBroadcastingStreamsView.as_view()),
     path("list_streams/", ListStreamsView.as_view()),
     path("spin_record/", SpinRecordView.as_view()),
     path("stream_subscription/", StreamSubscriptionView.as_view()),
