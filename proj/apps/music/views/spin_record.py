b588dabb361b5229c50bb76f9b41a37a9566a602
Josh Schultheiss <joshschultheiss@joshs-imac.lan>
2020-01-18 12:52:22 -0800

    more

--- proj/apps/music/views/spin_record.py
+++ proj/apps/music/views/spin_record.py
@@ -1,13 +1,11 @@
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
 
+from datetime import datetime
+
 from django.apps import apps
 
 from proj.core.views import BaseView
-from proj.apps.music.models import Comment
-from proj.apps.music.models import Ticket
-from proj.apps.music.models import Stream
-from proj.apps.users.models import Profile
 
 
 SUBSCRIBED = "subscribed"
@@ -16,7 +14,7 @@ UNSUBSCRIBED = "unsubscribed"
 
 @method_decorator(login_required, name="dispatch")
 class SpinRecordView(BaseView):
-    def put(self, request, **kwargs):
+    def post(self, request, **kwargs):
         """
         Update the user's account information.
         """
@@ -27,20 +25,25 @@ class SpinRecordView(BaseView):
         stream_uuid = request.POST.get("stream_uuid", None)
 
         now = datetime.now()
-        try:
-            pre_save_stream = Stream.objects.get(uuid=stream_uuid)
-            if pre_save_stream.current_record and pre_save_stream.record_terminates_at:
-                if now < pre_save_stream.record_terminates_at.replace(tzinfo=None):
-                    raise Exception(
-                        'The record cannot be changed since one is still '
-                        'playing.'
-                    )
-        except Stream.DoesNotExist:
-            raise Exception('Stream does not exist')
+        stream = Stream.objects.get(uuid=stream_uuid)
+
+        if stream.status != Stream.STATUS_ACTIVATED:
+            raise Exception('The stream is not active')
+
+        record_on_table = stream.current_record and stream.record_terminates_at
+        if record_on_table:
+            record_is_playing = (
+                now < stream.record_terminates_at.replace(tzinfo=None)
+            )
+            if record_is_playing:
+                raise Exception(
+                    'The record cannot be changed since one is still '
+                    'playing.'
+                )
 
         next_record = Record.objects.get(id=record_id)
-        pre_save_stream.record = next_record
-        pre_save_stream.save()
+        stream.record = next_record
+        stream.save()
 
         if not next_record:
             return
