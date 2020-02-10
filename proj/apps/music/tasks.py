a09b7031a8acf32dc758d84d6ccf34004ce69f19
Josh Schultheiss <joshschultheiss@joshs-imac.lan>
2020-02-10 00:32:33 -0800

    more

--- proj/apps/music/tasks.py
+++ proj/apps/music/tasks.py
@@ -5,6 +5,11 @@ from datetime import timedelta
 from django.apps import apps
 
 
+LOOPED_STREAM_IDS = [
+    16,  # 🍃
+]
+
+
 @shared_task
 def schedule_spin(stream_id):
     Queue = apps.get_model('music', 'Queue')
@@ -22,14 +27,24 @@ def schedule_spin(stream_id):
         .first()
     )
     if not queue:
-        return
+        if stream_id not in LOOPED_STREAM_IDS:
+            return
+        queue = (
+            Queue
+            .objects
+            .select_related('stream', 'record')
+            .filter(stream=stream)
+            .order_by("played_at")
+            .first()
+        )
 
     record = queue.record
 
     Stream.objects.spin(record, stream)
-    queue.delete()
 
     now = datetime.now()
+    queue.played_at = now
+
     next_play = now + timedelta(milliseconds=(record.duration_ms + 250))
 
     schedule_spin.apply_async(eta=next_play, args=[stream_id])
