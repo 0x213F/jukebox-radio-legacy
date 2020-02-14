6281f1d3101ce519727ad651d55ff3c7649b287b
Joshua Schultheiss <joshua.schultheiss@jyve.com>
2020-02-14 12:11:57 -0500

    update script

--- proj/apps/users/management/commands/refresh_spotify_tokens.py
+++ proj/apps/users/management/commands/refresh_spotify_tokens.py
@@ -16,26 +16,29 @@ class Command(BaseCommand):
         profiles_to_refresh = Profile.objects.filter(spotify_access_token__isnull=False)
 
         for profile in profiles_to_refresh:
-            cipher_suite = Fernet(settings.DATABASE_ENCRYPTION_KEY)
-            spotify_refresh_token = cipher_suite.decrypt(
-                profile.spotify_refresh_token.encode("utf-8")
-            ).decode("utf-8")
+            try:
+                cipher_suite = Fernet(settings.DATABASE_ENCRYPTION_KEY)
+                spotify_refresh_token = cipher_suite.decrypt(
+                    profile.spotify_refresh_token.encode("utf-8")
+                ).decode("utf-8")
 
-            response = requests.post(
-                "https://accounts.spotify.com/api/token",
-                data={
-                    "grant_type": "refresh_token",
-                    "refresh_token": spotify_refresh_token,
-                    "client_id": '133a25c7195344dbafd4f50d7450330f',
-                    "client_secret": '4029f523ad8a46cb86e29b9dd54cc257',
-                },
-            )
-            response_json = response.json()
+                response = requests.post(
+                    "https://accounts.spotify.com/api/token",
+                    data={
+                        "grant_type": "refresh_token",
+                        "refresh_token": spotify_refresh_token,
+                        "client_id": '133a25c7195344dbafd4f50d7450330f',
+                        "client_secret": '4029f523ad8a46cb86e29b9dd54cc257',
+                    },
+                )
+                response_json = response.json()
 
-            cipher_spotify_access_token = cipher_suite.encrypt(
-                response_json["access_token"].encode("utf-8")
-            )
+                cipher_spotify_access_token = cipher_suite.encrypt(
+                    response_json["access_token"].encode("utf-8")
+                )
 
-            profile.spotify_access_token = cipher_spotify_access_token.decode("utf-8")
-            profile.spotify_scope = response_json["scope"]
-            profile.save()
+                profile.spotify_access_token = cipher_spotify_access_token.decode("utf-8")
+                profile.spotify_scope = response_json["scope"]
+                profile.save()
+            except Exception:
+                pass
