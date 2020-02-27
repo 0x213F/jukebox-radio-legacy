1faa5429f8302feda212e2255076ed8897259d21
Josh Schultheiss <joshschultheiss@joshs-imac.lan>
2020-02-26 21:03:17 -0800

    adding play-bar and stream changing in home

--- proj/site/static/js/proj/home.js
+++ proj/site/static/js/proj/home.js
@@ -14,29 +14,27 @@ function generate_stream(stream) {
   }
   return `
   <div class="card-body broadcasting-stream" uuid="${stream.uuid}" style="cursor: pointer;">
-    <a href="/stream/${stream.uuid}" class="no-link-style">
-      <div class="card" style="margin-bottom: 0px;">
-        <div class="card-body">
+    <div class="card" style="margin-bottom: 0px;">
+      <div class="card-body">
 
-          <div class="form-group" style="line-height: 36px;">
-            <h5>${stream.name}</h5>
-          </div>
-
-          <div class="form-group" style="line-height: 36px;">
-            <div class="chip" style="border-radius: 28px">
-              <figure class="avatar avatar-sm" data-initial="" style="background-color: ${background_color};"></figure>${stream.owner_name}
-            </div>
-          </div>
-
-          <div class="divider"></div>
-
-          <div class="form-group" style="line-height: 36px;">
-            ${tags_html}
-          </div>
-
+        <div class="form-group" style="line-height: 36px;">
+          <h5>${stream.name}</h5>
         </div>
+
+        <div class="form-group" style="line-height: 36px;">
+          <div class="chip" style="border-radius: 28px">
+            <figure class="avatar avatar-sm" data-initial="" style="background-color: ${background_color};"></figure>${stream.owner_name}
+          </div>
+        </div>
+
+        <div class="divider"></div>
+
+        <div class="form-group" style="line-height: 36px;">
+          ${tags_html}
+        </div>
+
       </div>
-    </form>
+    </div>
   </div>
   `
 }
@@ -49,6 +47,8 @@ function display_tune_in_streams(data) {
   }
   setup_ajax_forms();
   $(".tune-in-streams").removeClass('hidden');
+
+  $('.card-body.broadcasting-stream > .card').click(activate_stream)
 }
 
 
@@ -60,4 +60,41 @@ function display_broadcasting_streams(data) {
   }
   setup_ajax_forms();
   $(".broadcasting-streams").removeClass('hidden');
+}
+
+// CLICK LISTENERS
+
+function activate_stream(data) {
+  var uuid = $(this).parent().attr('uuid');
+
+  $('.active-stream').removeClass('active-stream');
+  $(this).addClass('active-stream');
+
+  var endpoint = (
+    'ws://' + window.location.host + window.location.pathname +
+    `?uuid=${uuid}`
+  )
+
+  window['SOCKET'] = new WebSocket(endpoint)
+  window['SOCKET'].onopen = onopen
+  window['SOCKET'].onmessage = onmessage
+}
+
+$( document ).ready(function() {
+  $('#play-bar-chat-button').click(function(data) {
+    var uuid = $('.active-stream').parent().attr('uuid');
+    console.log(uuid)
+    window.location.href = `/stream/${uuid}`;
+  });
+});
+
+// WEBSOCKET FUNCTIONS
+
+function onopen(event) {
+  // NOOP
+}
+
+function onmessage(event) {
+  var $playBar = $('#play-bar');
+  $playBar.removeClass('hide-under-view');
 }
