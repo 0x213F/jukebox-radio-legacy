c6a6c4d4aaa606ff21b52dfca6c89508e48db3cb
Josh Schultheiss <joshschultheiss@joshs-imac.lan>
2020-03-07 11:53:18 -0800

    more gong

--- proj/site/views/addrecord.py
+++
@@ -1,8 +0,0 @@
-from django.template.response import TemplateResponse
-
-from proj.core.views import BaseView
-
-
-class AddRecordView(BaseView):
-    def get(self, request, **kwargs):
-        return TemplateResponse(request, "addrecord.html")
