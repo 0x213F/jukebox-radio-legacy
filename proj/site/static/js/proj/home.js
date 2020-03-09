c95a9451ecf17784554a62f32e1cd4a9e0d44cbb
Josh Schultheiss <joshschultheiss@joshs-imac.lan>
2020-03-09 00:31:14 -0700

    debug

--- proj/site/static/js/proj/home.js
+++ proj/site/static/js/proj/home.js
@@ -65,8 +65,10 @@ function display_tune_in_streams(data) {
 }
 
 function display_broadcasting_streams(data) {
+  console.log(data)
   let list_streams = data[KEY_SHOWINGS];
-  if(!list_streams) {
+  console.log(data)
+  if(!list_streams.length) {
     return;
   }
   let $streams_container = $('.broadcasting-streams');
