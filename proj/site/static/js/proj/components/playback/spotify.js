ebdfd8d7254b7ae3ad53e8d42f87c5dfa59f810f
Josh <josh@imac.lan>
2020-05-02 23:38:32 -0700

    fixed spotify playback kinda

--- proj/site/static/js/proj/components/playback/spotify.js
+++ proj/site/static/js/proj/components/playback/spotify.js
@@ -46,15 +46,41 @@ function syncSpotifyPlayback() {
     last_queue_listing = queue_listing;
   }
 
-  fetch(`https://api.spotify.com/v1/me/player/play`, {
-    method: 'PUT',
-    body: JSON.stringify({ uris: spotify_uris, position_ms: offset }),
+  fetch(`https://api.spotify.com/v1/me/player/currently-playing`, {
+    method: 'GET',
     headers: {
       'Content-Type': 'application/json',
       'Authorization': `Bearer ${playback.spotify_token}`
     },
-  });
+  })
+  .then(response => response.json())
+  .then(function(response_json) {
+    var timestamp = response_json.timestamp;
+    var spotify_uri = response_json.item.uri;
+    var duration_ms = response_json.item.duration_ms;
+    var is_playing = response_json.is_playing;
+
+    var newNow = new Date(timestamp);
+    var deltaSincePlayMs = newNow.getTime() - newNow;
+
+    var kindaClose = Math.abs(duration_ms - offset);
+
+    if(spotify_uri === spotify_uris[0] && is_playing && kindaClose < 1000) {
+      return;
+    }
+
+    fetch(`https://api.spotify.com/v1/me/player/play`, {
+      method: 'PUT',
+      body: JSON.stringify({ uris: spotify_uris, position_ms: offset }),
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${playback.spotify_token}`
+      },
+    });
+  })
 }
+
+https://api.spotify.com/v1/me/player/currently-playing
 
 
 function playTracks(spotify_uris, sat) {
